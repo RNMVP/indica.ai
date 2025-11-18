2bcc0d38be549aa3339f10fc42fed93243ec22ad
Pedro Gabriel <pedrogabriellima@gmail.com>
2025-11-18 14:36:27 -0300

    feat(auth): add AuthContext and useAuth hook for managing user authentication

---
+++ features/authentication/contexts/AuthContext.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { createContext, useEffect, useState } from "react";
+import { User } from "../models/user";
+import { RegisterDTO } from "@/lib/schemas/register.schema";
+import { LoginDTO } from "@/lib/schemas/login.schema";
+import { useRouter } from "next/navigation";
+
+interface AuthContextProps {
+  user: User | null;
+  loading: boolean;
+  login: (data: LoginDTO) => Promise<void>;
+  register: (data: RegisterDTO) => Promise<void>;
+  logout: () => void;
+}
+
+export const AuthContext = createContext<AuthContextProps | null>(null);
+
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const stored = localStorage.getItem("auth:user");
+    if (stored) setUser(JSON.parse(stored));
+    setLoading(false);
+  }, []);
+
+  async function login(data: LoginDTO) {
+    setLoading(true);
+
+    const user = {
+        id: "teste-id",
+        name: "Teste Usuário",
+        email: data.email,
+    };
+
+    setUser(user);
+    localStorage.setItem("auth:user", JSON.stringify(user));
+
+    setLoading(false);
+  }
+
+  async function register(data: RegisterDTO) {
+    setLoading(true);
+
+    // const newUser = {
+    //   id: "teste-id",
+    //   name: data.name,
+    //   email: data.email,
+    //   password: data.password,
+    // };
+
+    router.push("/auth/login");
+
+    setLoading(false);
+  }
+
+  function logout() {
+    setUser(null);
+    localStorage.removeItem("auth:user");
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, loading, login, register, logout }}>
+      { children }
+    </AuthContext.Provider>
+  );
+}
